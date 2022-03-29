571ad2526a83580ca36493b4df5891c3002e5d1d
Cosmin Tudorache <cosmin.tudorache@bolt.eu>
2022-03-29 12:58:12 +0300

    Move Generators to separate classes to util/

---
+++ contribs/taxi/src/test/java/org/matsim/contrib/taxi/rides/util/GridNetworkGenerator.java
@@ -0,0 +1,92 @@
+package org.matsim.contrib.taxi.rides.util;
+
+import org.matsim.api.core.v01.Coord;
+import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.network.Link;
+import org.matsim.api.core.v01.network.Network;
+import org.matsim.api.core.v01.network.Node;
+import org.matsim.core.network.NetworkUtils;
+import org.matsim.core.utils.misc.Time;
+
+import java.util.Map;
+
+public class GridNetworkGenerator {
+	final Network network;
+
+	public static enum Param {
+		NETWORK_CAPACITY_PERIOD,
+		LINK_LENGTH,
+		LINK_FREE_SPEED,
+		LINK_CAPACITY,
+		LINK_LANES
+	};
+	public static final String DEFAULT_NETWORK_CAPACITY_PERIOD = "1:00:00";
+	public static final Double DEFAULT_LINK_LENGTH = 100.0;
+	public static final Double DEFAULT_LINK_FREE_SPEED = 15.0;
+	public static final Double DEFAULT_LINK_CAPACITY = 100.0;
+	public static final Double DEFAULT_LINK_LANES = 1.0;
+
+	final double linkLength;
+	final double linkFreeSpeed;
+	final double linkCapacity;
+	final double linkLanes;
+
+	public GridNetworkGenerator(Network network, int xNodes, int yNodes, Map<Param, Object> params) {
+		this.network = network;
+		this.network.setCapacityPeriod(Time.parseTime((String)params.getOrDefault(Param.NETWORK_CAPACITY_PERIOD, DEFAULT_NETWORK_CAPACITY_PERIOD)));
+		this.linkLength = (Double)params.getOrDefault(Param.LINK_LENGTH, DEFAULT_LINK_LENGTH);
+		this.linkFreeSpeed = (Double)params.getOrDefault(Param.LINK_FREE_SPEED, DEFAULT_LINK_FREE_SPEED);
+		this.linkCapacity = (Double)params.getOrDefault(Param.LINK_CAPACITY, DEFAULT_LINK_CAPACITY);
+		this.linkLanes = (Double)params.getOrDefault(Param.LINK_LANES, DEFAULT_LINK_LANES);
+		this.build(xNodes, yNodes);
+	}
+
+	public Id<Node> nodeId(int x, int y) {
+		return Id.create(String.format("%d_%d", x, y), Node.class);
+	}
+
+	public Node node(int x, int y) {
+		return network.getNodes().get(nodeId(x, y));
+	}
+
+	public Id<Link> linkId(Node from, Node to) {
+		return Id.create(from.getId() + "-" + to.getId(), Link.class);
+	}
+
+	public Id<Link> linkId(int fromX, int fromY, int toX, int toY) {
+		return linkId(node(fromX, fromY), node(toX, toY));
+	}
+	public Coord nodeCoord(int x, int y) {
+		return new Coord(x * 1000, y * 1000);
+	}
+
+	private void addLink(Node a, Node b) {
+		NetworkUtils.createAndAddLink(network, linkId(a, b), a, b, linkLength, linkFreeSpeed, linkCapacity,  linkLanes);
+	}
+
+	private void addDoubleLink(Node a, Node b) {
+		addLink(a, b);
+		addLink(b, a);
+	}
+
+	private void build(int xNodes, int yNodes) {
+		for (int x = 0; x < xNodes; ++x) {
+			for (int y = 0; y < yNodes; ++y) {
+				NetworkUtils.createAndAddNode(network, nodeId(x, y), nodeCoord(x, y));
+			}
+		}
+		for (int x = 0; x < xNodes; ++x) {
+			for (int y = 0; y < yNodes; ++y) {
+				Node crtNode = node(x, y);
+				Node leftNode = node(x-1, y);
+				Node downNode = node(x, y-1);
+				if (leftNode != null) {
+					addDoubleLink(leftNode, crtNode);
+				}
+				if (downNode != null) {
+					addDoubleLink(downNode, crtNode);
+				}
+			}
+		}
+	}
+}
---
+++ contribs/taxi/src/test/java/org/matsim/contrib/taxi/rides/util/TestScenarioGenerator.java
@@ -0,0 +1,167 @@
+package org.matsim.contrib.taxi.rides.util;
+
+import org.apache.log4j.Logger;
+import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.TransportMode;
+import org.matsim.api.core.v01.network.Link;
+import org.matsim.api.core.v01.population.Activity;
+import org.matsim.api.core.v01.population.Leg;
+import org.matsim.api.core.v01.population.Person;
+import org.matsim.api.core.v01.population.Plan;
+import org.matsim.contrib.dvrp.analysis.ExecutedScheduleCollector;
+import org.matsim.contrib.dvrp.benchmark.DvrpBenchmarks;
+import org.matsim.contrib.dvrp.run.AbstractDvrpModeModule;
+import org.matsim.contrib.dvrp.run.DvrpConfigGroup;
+import org.matsim.contrib.dvrp.run.DvrpQSimComponents;
+import org.matsim.contrib.taxi.analysis.TaxiEventSequenceCollector;
+import org.matsim.contrib.taxi.benchmark.TaxiBenchmarkStats;
+import org.matsim.contrib.taxi.optimizer.rules.RuleBasedTaxiOptimizerParams;
+import org.matsim.contrib.taxi.run.MultiModeTaxiConfigGroup;
+import org.matsim.contrib.taxi.run.MultiModeTaxiModule;
+import org.matsim.contrib.taxi.run.TaxiConfigGroup;
+import org.matsim.core.config.Config;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.config.groups.QSimConfigGroup;
+import org.matsim.core.controler.Controler;
+import org.matsim.core.controler.OutputDirectoryHierarchy;
+import org.matsim.core.population.PersonUtils;
+import org.matsim.core.population.PopulationUtils;
+import org.matsim.core.scenario.ScenarioUtils;
+import org.matsim.vehicles.Vehicle;
+import org.matsim.vehicles.VehicleType;
+import org.matsim.vehicles.VehicleUtils;
+
+import java.util.Map;
+
+public class TestScenarioGenerator {
+	private static final Logger log = Logger.getLogger(TestScenarioGenerator.class);
+
+	private Config config;
+	private Scenario scenario;
+
+	public TestScenarioGenerator() {
+		buildConfig();
+	}
+
+	private void buildConfig() {
+		TaxiConfigGroup taxiCfgGen = new TaxiConfigGroup();
+		taxiCfgGen.setBreakSimulationIfNotAllRequestsServed(false);
+		taxiCfgGen.setDestinationKnown(false);
+		taxiCfgGen.setVehicleDiversion(false);
+		taxiCfgGen.setPickupDuration(120);
+		taxiCfgGen.setDropoffDuration(60);
+		taxiCfgGen.setTimeProfiles(true);
+		taxiCfgGen.setDetailedStats(true);
+
+		RuleBasedTaxiOptimizerParams ruleParams = new RuleBasedTaxiOptimizerParams();
+		ruleParams.setReoptimizationTimeStep(1);
+		ruleParams.setNearestVehiclesLimit(30);
+		taxiCfgGen.addParameterSet(ruleParams);
+		log.warn("CTudorache taxiCfgGen: " + taxiCfgGen);
+
+		MultiModeTaxiConfigGroup multiModeTaxiConfigGroup = new MultiModeTaxiConfigGroup();
+		multiModeTaxiConfigGroup.addParameterSet(taxiCfgGen);
+
+		config = ConfigUtils.createConfig(multiModeTaxiConfigGroup, new DvrpConfigGroup());
+		log.warn("CTudorache modules: " + config.getModules().keySet());
+
+
+		config.controler().setOutputDirectory("test/output/abcdef");
+		config.controler().setOverwriteFileSetting(OutputDirectoryHierarchy.OverwriteFileSetting.deleteDirectoryIfExists);
+		config.controler().setLastIteration(0);
+		config.controler().setDumpDataAtEnd(false);
+		config.controler().setWriteEventsInterval(0);
+		config.controler().setWritePlansInterval(0);
+		config.controler().setCreateGraphs(false);
+		config.qsim().setSimStarttimeInterpretation(QSimConfigGroup.StarttimeInterpretation.onlyUseStarttime);
+		config.qsim().setInsertingWaitingVehiclesBeforeDrivingVehicles(true);
+		config.qsim().setSnapshotStyle(QSimConfigGroup.SnapshotStyle.queue);
+		config.planCalcScore().addParam("activityType_0", "dummy");
+		config.planCalcScore().addParam("activityTypicalDuration_0", "24:00:00");
+		config.planCalcScore().addParam("traveling_taxi", "-6");
+		config.planCalcScore().addParam("activityTypicalDuration_1", "28800");
+
+		DvrpBenchmarks.adjustConfig(config);
+
+		// create scenario
+		scenario = ScenarioUtils.createScenario(config);
+
+		// sanity check
+		log.warn("CTudorache resultTaxiCfg: " + getTaxiCfg());
+		log.warn("CTudorache resultRuleParams: " + getRuleBasedTaxiOptimizerParams());
+	}
+
+	// TODO(CTudorache): could be moved to buildConfig(), after scenario creation
+	public GridNetworkGenerator buildGridNetwork(int xNodes, int yNodes) {
+		return new GridNetworkGenerator(scenario.getNetwork(), xNodes, yNodes, Map.of());
+	}
+
+	public Config getConfig() {
+		return config;
+	}
+	public TaxiConfigGroup getTaxiCfg() {
+		return TaxiConfigGroup.getSingleModeTaxiConfig(config);
+	}
+	public RuleBasedTaxiOptimizerParams getRuleBasedTaxiOptimizerParams() {
+		TaxiConfigGroup taxiCfg = getTaxiCfg();
+		return (RuleBasedTaxiOptimizerParams) taxiCfg.getTaxiOptimizerParams();
+	}
+
+	public void addPassenger(int passengerId, Id<Link> fromLink, Id<Link> toLink, Double departureTime) {
+		// TODO(CTudorache): utility method for generating passenger id: "passenger_%d"
+		Person person = PopulationUtils.getFactory().createPerson(Id.create("passenger_" + passengerId, Person.class));
+		PersonUtils.setEmployed(person, false);
+
+		Plan plan = PopulationUtils.createPlan(person);
+		person.addPlan(plan);
+		person.setSelectedPlan(plan);
+
+		Activity a = PopulationUtils.createAndAddActivityFromLinkId(plan, "dummy", fromLink);
+		a.setEndTime(departureTime);
+
+		Leg leg = PopulationUtils.createAndAddLeg(plan, TransportMode.taxi);
+
+		PopulationUtils.createAndAddActivityFromLinkId(plan, "dummy", toLink);
+		scenario.getPopulation().addPerson(person);
+	}
+
+	public void addVehicle(int vehicleId, Id<Link> startLink, Double t0, Double t1) {
+		// TODO(CTudorache): vehType needs to be constant
+		VehicleType vehType = VehicleUtils.createVehicleType(Id.create("taxiType", VehicleType.class));
+		vehType.getCapacity().setSeats(4);
+		scenario.getVehicles().addVehicleType(vehType);
+
+		// TODO(CTudorache): utility method for generating vehicle id: "taxi_vehicle_%d"
+		Vehicle v = VehicleUtils.createVehicle(Id.create("taxi_vehicle_" + vehicleId, Vehicle.class), vehType);
+		// TODO(CTudorache): do these attributes work? could not find an example of generating vehicle with start_link, t_0, t_1
+		v.getAttributes().putAttribute("dvrpMode", "taxi");
+		v.getAttributes().putAttribute("startLink", startLink.toString());
+		v.getAttributes().putAttribute("serviceBeginTime", t0);
+		v.getAttributes().putAttribute("serviceEndTime", t1);
+		scenario.getVehicles().addVehicle(v);
+	}
+
+	public Controler createController() {
+		Controler controler = new Controler(scenario);
+		DvrpBenchmarks.initController(controler);
+
+		final String mode = TransportMode.taxi;
+		controler.configureQSimComponents(DvrpQSimComponents.activateModes(mode));
+
+		controler.addOverridingModule(new MultiModeTaxiModule());
+
+		controler.addOverridingModule(new AbstractDvrpModeModule(mode) {
+			@Override
+			public void install() {
+				bindModal(TaxiBenchmarkStats.class).toProvider(modalProvider(
+						getter -> new TaxiBenchmarkStats(getter.get(OutputDirectoryHierarchy.class),
+								getter.getModal(ExecutedScheduleCollector.class),
+								getter.getModal(TaxiEventSequenceCollector.class)))).asEagerSingleton();
+				addControlerListenerBinding().to(modalKey(TaxiBenchmarkStats.class));
+			}
+		});
+
+		return controler;
+	}
+}
